3e8bd3a49e02186f74dadbad67efa44a48af0011
Lucas <suporte.ti@tca.digital>
2024-11-04 15:45:53 -0300

    Commit inicial

---
+++ TranfereFaqParaDepartamentos.bat
@@ -0,0 +1,12 @@
+
+copy C:\"Troca de arquivos"\TI\FAQ-TCA\"FAQ TCA.html" "C:\Troca de arquivos\ADMINISTRATIVO\TI\FAQ-TCA"
+
+copy C:\"Troca de arquivos"\TI\FAQ-TCA\"FAQ TCA.html" "C:\Troca de arquivos\FISCAL\TI\FAQ-TCA"
+
+copy C:\"Troca de arquivos"\TI\FAQ-TCA\"FAQ TCA.html" "C:\Troca de arquivos\CONTABIL\TI\FAQ-TCA"
+
+copy C:\"Troca de arquivos"\TI\FAQ-TCA\"FAQ TCA.html" "C:\Troca de arquivos\DP\TI\FAQ-TCA"
+
+
+
+pause;
---
+++ startAcessoriasAdm.bat
@@ -0,0 +1,14 @@
+@echo off
+
+cd\
+cd Arquivos de Programas(x86)\Google\Chrome\Application
+
+start chrome.exe https://app.acessorias.com/sysmain.php?m=147
+
+timeout t/5
+
+start C:\"Troca de arquivos"\ADMINISTRATIVO\TI\FAQ-TCA\"FAQ TCA".html
+
+exit
+
+pause;
